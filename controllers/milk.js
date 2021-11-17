f8db420be2a0d1b780f104321c255cc7e92a46bc
Nikhil Porika <nikhil.porika36@gmail.com>
2021-11-16 23:01:22 -0600

    Pushing code

--- controllers/milk.js
+++ controllers/milk.js
@@ -4,6 +4,7 @@ exports.milk_list = function (req, res) {
     res.send('NOT IMPLEMENTED: Milk list');
 };
 // for a specific Milk.
+// for a specific Milk.
 exports.milk_detail = async function(req, res) {
     console.log("detail" + req.params.id)
     try {
@@ -13,8 +14,7 @@ exports.milk_detail = async function(req, res) {
     res.status(500)
     res.send(`{"error": document for id ${req.params.id} not found`);
     }
-    };
-
+   };
 // Handle Milk create on POST.
 exports.milk_create_post = async function (req, res) {
     console.log(req.body)
@@ -22,7 +22,7 @@ exports.milk_create_post = async function (req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"milk_type":"regular", "quantity":13, "cost":43.56}
+    // {"gas_type":"regular", "quantity":13, "cost":43.56}
     document.milk_type = req.body.milk_type;
     document.quantity = req.body.quantity;
     document.cost = req.body.cost;
@@ -34,30 +34,37 @@ exports.milk_create_post = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
-// Handle Milk delete form on DELETE.
-exports.milk_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Milk delete DELETE ' + req.params.id);
-};
-// Handle Milk update form on PUT.
+// Handle Milk delete on DELETE.
+exports.milk_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Milk.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
 // Handle Milk update form on PUT.
 exports.milk_update_put = async function(req, res) {
-console.log(`update on id ${req.params.id} with body
+ console.log(`update on id ${req.params.id} with body
 ${JSON.stringify(req.body)}`)
-try {
-let toUpdate = await Milk.findById( req.params.id)
-// Do updates of properties
-if(req.body.costume_type)
-toUpdate.milk_type = req.body.milk_type;
-if(req.body.quantity) toUpdate.cost = req.body.quantity;
-if(req.body.cost) toUpdate.cost = req.body.cost;
-let result = await toUpdate.save();
-console.log("Sucess " + result)
-res.send(result)
-} catch (err) {
-res.status(500)
-res.send(`{"error": ${err}: Update for id ${req.params.id}
+ try {
+ let toUpdate = await Milk.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.milk_type)
+ toUpdate.milk_type = req.body.milk_type;
+ if(req.body.quantity) toUpdate.quantity = req.body.quantity;
+ if(req.body.cost) toUpdate.cost = req.body.cost;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
 failed`);
-}
+ }
 };
 
 // List of all Milk
@@ -85,3 +92,56 @@ exports.milk_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+// Handle a show one view with id specified by query
+exports.milk_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Milk.findById( req.query.id)
+    res.render('milkdetail',
+   { title: 'Milk Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+// Handle building the view for creating a gas.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.milk_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('milkcreate', { title: 'Milk Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+// Handle building the view for updating a gas.
+// query provides the id
+exports.milk_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Milk.findById(req.query.id)
+    res.render('milkupdate', { title: 'Milk Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+// Handle a delete one view with id from query
+exports.milk_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Milk.findById(req.query.id)
+    res.render('milkdelete', { title: 'Milk Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/milk.js
+++ routes/milk.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const milk_controlers= require('../controllers/milk');
 var router = express.Router();
-/* GET costumes */
+/* GET gas */
 router.get('/', milk_controlers.milk_view_all_Page );
+/* GET detail gas page */
+router.get('/detail', milk_controlers.milk_view_one_Page);
+/* GET create gas page */
+router.get('/create', milk_controlers.milk_create_Page);
+/* GET create update page */
+router.get('/update', milk_controlers.milk_update_Page);
+/* GET create gas page */
+router.get('/delete', milk_controlers.milk_delete_Page);
+
+
 module.exports = router;
